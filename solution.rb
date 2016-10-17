dc5aed3ca1fafc932eb217f0c526d46c3ad2d940
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-10-16 20:31:11 -0500

    reto6

---
+++ solution.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/cookies'
+
+get '/' do
+
+	unless cookies[:suma]
+      cookies[:suma] = 0
+      end
+
+      @suma = cookies[:suma]
+      
+	  erb :index
+end
+
+post '/' do
+  cookies[:suma]= cookies[:suma].to_i+1
+  @suma = cookies[:suma]
+  erb :index
+end
